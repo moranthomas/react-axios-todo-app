759bbd874e73fe1b4581cfca0dd2f2f837e58500
moranthomas <thomas@optimizme.com>
2020-05-16 17:33:06 +0100

    running code from tutorial using axios

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import './App.css';
+import Todos from './components/Todos';
+import AddTodo from './components/AddTodo';
+import Header from './layout/Header';
+//import {v5 as uuid} from "uuid";
+import About from './components/pages/About';
+
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import axios from 'axios';
+
+class App extends React.Component {
+
+  state = {
+    todos: [
+    ]
+  }
+
+  componentDidMount() {
+    axios.get('https://jsonplaceholder.typicode.com/todos?_limit=10').then(res => this.setState( {todos: res.data}))
+  }
+
+  markComplete = (id) => {
+   // console.log(id)
+   this.setState ( {todos: this.state.todos.map( todo => {
+     if(todo.id === id) {
+       todo.completed =!todo.completed
+     }
+     return todo;
+   })})
+  }
+
+  delTodo = (id) => {
+    //console.log(id)
+    axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+    .then( res => this.setState ( {todos: [...this.state.todos.filter(todo => todo.id !== id)]}))
+  }
+
+  addTodo = (title) => {
+    axios.post('https://jsonplaceholder.typicode.com/todos', {
+      title: title,
+      completed: false
+    }).then(res => this.setState( {todos: [...this.state.todos, res.data] }));
+  }
+
+  render() {
+    console.log('from app.js');
+
+    return (
+      <Router>
+      <div className="App">
+        <div className="container">
+          <Header />
+          <Route exact path="/" render={props => (
+            <React.Fragment>
+              <AddTodo addTodo={this.addTodo} />
+              <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo}/>
+            </React.Fragment>
+          )} />
+
+          <Route path="/about" component={About} />
+       </div>
+      </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/pages/About.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class About extends Component {
+    render() {
+        return (
+          <React.Fragment>
+<h1> ABout </h1>
+
+          </React.Fragment>
+        )
+    }
+}
+
+export default About
